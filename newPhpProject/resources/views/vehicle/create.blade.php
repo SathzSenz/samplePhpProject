ce4c215065f677b4eed304fe951ea81d59e33fb7
SathSenz <sathinsenadeera@gmail.com>
2024-11-26 12:52:54 +0530

    added create and show vehicles views

---
+++ newPhpProject/resources/views/vehicle/create.blade.php
@@ -0,0 +1,48 @@
+@extends('vehicle.layout')
+
+@section('content')
+
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">
+                        <h4>Add New Vehicle
+                            <a href="{{ url('vehicle') }}" class="btn btn-danger float-end">Back</a>
+                        </h4>
+                    </div>
+                    <div class="card-body">
+                        <form action="{{ route('vehicle.store') }}" method="POST">
+                            @csrf
+
+                            <div class="mb-3">
+                                <label>Make</label>
+                                <input type="text" name="make" class="form-control" />
+                                @error('make') <span class="text-danger">{{ $message }}</span> @enderror
+                            </div>
+                            <div class="mb-3">
+                                <label>Model</label>
+                                <input type="text" name="model" class="form-control" />
+                                @error('model') <span class="text-danger">{{ $message }}</span> @enderror
+                            </div>
+                            <div class="mb-3">
+                                <label>Year</label>
+                                <input type="number" name="year" class="form-control" />
+                                @error('year') <span class="text-danger">{{ $message }}</span> @enderror
+                            </div>
+                            <div class="mb-3">
+                                <label>License Plate</label>
+                                <input type="text" name="license_plate" class="form-control" />
+                                @error('license_plate') <span class="text-danger">{{ $message }}</span> @enderror
+                            </div>
+                            <div class="mb-3">
+                                <button type="submit" class="btn btn-primary">Save</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+@endsection
---
+++ newPhpProject/resources/views/vehicle/show.blade.php
@@ -0,0 +1,45 @@
+@extends('vehicle.layout')
+
+@section('content')
+
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">
+                        <h4>Show Vehicle Details
+                            <a href="{{ url('vehicle') }}" class="btn btn-danger float-end">Back</a>
+                        </h4>
+                    </div>
+                    <div class="card-body">
+                        <div class="mb-3">
+                            <label>Make</label>
+                            <p>
+                                {{ $vehicle->make }}
+                            </p>
+                        </div>
+                        <div class="mb-3">
+                            <label>Model</label>
+                            <p>
+                                {{ $vehicle->model }}
+                            </p>
+                        </div>
+                        <div class="mb-3">
+                            <label>Year</label>
+                            <p>
+                                {{ $vehicle->year }}
+                            </p>
+                        </div>
+                        <div class="mb-3">
+                            <label>License Plate</label>
+                            <p>
+                                {{ $vehicle->license_plate }}
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+@endsection
