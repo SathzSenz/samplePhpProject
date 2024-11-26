30a39141be562974a23fa88212a98d27dde6d92b
SathSenz <sathinsenadeera@gmail.com>
2024-11-26 21:49:11 +0530

    integrated create and edit maintenance views

---
+++ newPhpProject/resources/views/maintenance/create.blade.php
@@ -0,0 +1,22 @@
+@extends('maintenance.layout')
+
+@section('content')
+    <h1>Add Maintenance for {{ $vehicle->make }} {{ $vehicle->model }}</h1>
+
+    <form action="{{ route('vehicle.maintenance.store', $vehicle) }}" method="POST">
+        @csrf
+        <div class="form-group">
+            <label for="description">Description</label>
+            <input type="text" name="description" class="form-control" required>
+        </div>
+        <div class="form-group">
+            <label for="date">Date</label>
+            <input type="date" name="date" class="form-control" required>
+        </div>
+        <div class="form-group">
+            <label for="cost">Cost</label>
+            <input type="number" name="cost" class="form-control" step="0.01" required>
+        </div>
+        <button type="submit" class="btn btn-success">Save</button>
+    </form>
+@endsection
---
+++ newPhpProject/resources/views/maintenance/edit.blade.php
@@ -0,0 +1,23 @@
+@extends('maintenance.layout')
+
+@section('content')
+    <h1>Edit Maintenance for {{ $vehicle->make }} {{ $vehicle->model }}</h1>
+
+    <form action="{{ route('vehicle.maintenance.update', [$vehicle, $maintenance]) }}" method="POST">
+        @csrf
+        @method('PUT')
+        <div class="form-group">
+            <label for="description">Description</label>
+            <input type="text" name="description" class="form-control" value="{{ $maintenance->description }}" required>
+        </div>
+        <div class="form-group">
+            <label for="date">Date</label>
+            <input type="date" name="date" class="form-control" value="{{ $maintenance->date }}" required>
+        </div>
+        <div class="form-group">
+            <label for="cost">Cost</label>
+            <input type="number" name="cost" class="form-control" value="{{ $maintenance->cost }}" step="0.01" required>
+        </div>
+        <button type="submit" class="btn btn-success">Update</button>
+    </form>
+@endsection
