b428ca6cacb08a12be6a1e66556707f198377ad6
SathSenz <sathinsenadeera@gmail.com>
2024-11-27 09:52:02 +0530

    altered edit maintenance view

--- newPhpProject/resources/views/maintenance/edit.blade.php
+++ newPhpProject/resources/views/maintenance/edit.blade.php
@@ -1,23 +1,35 @@
 @extends('admin.layout')
 
 @section('content')
-    <h3>Edit Maintenance for {{ $vehicle->make }} {{ $vehicle->model }} ({{ $vehicle->license_plate }})</h3>
 
-    <form action="{{ route('vehicle.maintenance.update', [$vehicle, $maintenance]) }}" method="POST">
-        @csrf
-        @method('PUT')
-        <div class="form-group">
-            <label for="description">Description</label>
-            <input type="text" name="description" class="form-control" value="{{ $maintenance->description }}" required>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">
+                        <h4>Edit Maintenance for {{ $vehicle->make }} {{ $vehicle->model }} ({{ $vehicle->license_plate }})</h4>
+                    </div>
+                    <div class="card-body">
+                        <form action="{{ route('vehicle.maintenance.update', [$vehicle, $maintenance]) }}" method="POST">
+                            @csrf
+                            @method('PUT')
+                            <div class="mb-3">
+                                <label for="description">Description</label>
+                                <input type="text" name="description" class="form-control" value="{{ $maintenance->description }}" required>
+                            </div>
+                            <div class="mb-3">
+                                <label for="date">Date</label>
+                                <input type="date" name="date" class="form-control" value="{{ $maintenance->date }}" required>
+                            </div>
+                            <div class="mb-3">
+                                <label for="cost">Cost</label>
+                                <input type="number" name="cost" class="form-control" value="{{ $maintenance->cost }}" step="0.01" required>
+                            </div>
+                            <button type="submit" class="btn btn-success">Update</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
         </div>
-        <div class="form-group">
-            <label for="date">Date</label>
-            <input type="date" name="date" class="form-control" value="{{ $maintenance->date }}" required>
-        </div>
-        <div class="form-group">
-            <label for="cost">Cost</label>
-            <input type="number" name="cost" class="form-control" value="{{ $maintenance->cost }}" step="0.01" required>
-        </div>
-        <button type="submit" class="btn btn-success">Update</button>
-    </form>
+    </div>
 @endsection
