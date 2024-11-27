fe0781acf59f113b190880145f57e9c13d9975e1
SathSenz <sathinsenadeera@gmail.com>
2024-11-27 11:36:44 +0530

    added sidebar css

--- newPhpProject/resources/views/components/sidebar.blade.php
+++ newPhpProject/resources/views/components/sidebar.blade.php
@@ -5,7 +5,7 @@
     <h2 class="text-center py-3 d-md-none">Dashboard</h2>
     <ul class="nav flex-column">
         <li class="nav-item border-bottom border-light mb-2 mt-2">
-            <a class="nav-link text-white {{ request()->is('dashboard') ? 'active' : '' }}" href="#">Dashboard</a>
+            <a class="nav-link text-white {{ request()->is('dashboard') ? 'active' : '' }}" href="{{ route('admin.dashboard') }}">Dashboard</a>
         </li>
         <li class="nav-item border-bottom border-light mb-2 mt-2">
             <a class="nav-link text-white {{ request()->routeIs('vehicle.index') ? 'active' : '' }}" href="{{ route('vehicle.index') }}">
